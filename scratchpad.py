3a2d7f9fa6036f5bb92cdcc7f5c8828959b00a57
Jennifer Walker <jenfly@gmail.com>
2015-09-16 16:31:43 -0700

    More testing scripts

--- scratchpad.py
+++ scratchpad.py
@@ -15,60 +15,7 @@ from atmos.utils import print_if
 from atmos.constants import const as constants
 
 # ----------------------------------------------------------------------
-# Read monthly mean climatologies and do some test calcs
-# ----------------------------------------------------------------------
-
-filename = 'data/more/ncep2_climatology_monthly.nc'
-ds = dat.ncload(filename)
-lat = ds['lat'].values
-lon = ds['lon'].values
-plev = ds['plev'].values
-mon = ds['mon'].values
-u = ds['u']
-v = ds['v']
-T = ds['T']
-ps = ds['ps']
-
-topo = dat.get_ps_clim(lat, lon) / 100
-topo.units = 'hPa'
-
-# ----------------------------------------------------------------------
-# Correct for topography
-
-u_orig = u
-u = dat.correct_for_topography(u_orig, topo)
-
-m, k = 3, 1
-plt.figure(figsize=(7,8))
-plt.subplot(211)
-ap.pcolor_latlon(u_orig[m,k], cmap='jet')
-plt.subplot(212)
-ap.pcolor_latlon(u[m,k], cmap='jet')
-
-# ----------------------------------------------------------------------
-
-# Zonal mean zonal wind
-season='jjas'
-lon1, lon2 = 60, 100
-cint = 5
-months = utils.season_months(season)
-
-uplot = dat.subset(u, 'lon', lon1, lon2, 'mon', months)
-uplot = uplot.mean(['lon', 'mon'])
-
-ps_plot = dat.subset(topo, 'lon', lon1, lon2)
-ps_plot = ps_plot.mean('lon')
-
-plt.figure()
-ap.contour_latpres(uplot, clev=cint, topo=ps_plot)
-
-plt.figure()
-ap.contourf_latpres(uplot,clev=cint, topo=ps_plot)
-
-# ======================================================================
-# OpenDAP
-
-# Use xray to open an HDF OpenDAP file!
+# Read some data from OpenDAP url
 
 url = ('http://goldsmr3.sci.gsfc.nasa.gov/opendap/MERRA_MONTHLY/'
     'MAIMCPASM.5.2.0/1979/MERRA100.prod.assim.instM_3d_asm_Cp.197901.hdf')
@@ -79,49 +26,8 @@ ps = ds['PS']
 q = ds['QV']
 plev = dat.get_plev(T, units='Pa')
 lat, lon = dat.get_lat(ps), dat.get_lon(ps)
-p0 = 1e5
-
-theta = av.potential_temp(T, plev, p0)
-theta_e = av.equiv_potential_temp(T, plev, q, p0)
 
 # t, k = 0, 6
 # t, k = 0, 22
 t, k = 0, 14
 pstr = '%d hPa' % (plev[k]/100)
-plt.figure(figsize=(7,10))
-plt.subplot(311)
-ap.pcolor_latlon(T[t,k])
-plt.title('Temperature ' + pstr)
-plt.subplot(312)
-ap.pcolor_latlon(theta[t,k])
-plt.title('Potential Temperature ' + pstr)
-plt.subplot(313)
-ap.pcolor_latlon(theta_e[t,k])
-plt.title('Equiv Potential Temperature ' + pstr)
-
-ps.dims
-plt.figure()
-ap.pcolor_latlon(ps,cmap='hot')
-
-
-# ----------------------------------------------------------------------
-
-
-# ----------------------------------------------------------------------
-
-
-# ----------------------------------------------------------------------
-# More testing of mean_over_geobox
-
-#lat1, lat2 = 10, 60
-lat1, lat2 = -88, -60
-lon1, lon2 = 20, 120
-lat = np.arange(89., -89., -1.0)
-lon = np.arange(0., 359., 1.)
-data1 = 10 - np.radians(lat*2)**2
-data = np.reshape(data1, (178, 1))
-data = np.tile(data, (10, 1, 359))
-
-plt.figure()
-plt.plot(lat, data[0].mean(axis=-1))
-plt.xlim(lat1, lat2)
