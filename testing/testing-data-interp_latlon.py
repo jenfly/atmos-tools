dcdd83bc1f75cc4ad53575730a503b484b89e47d
Jennifer Walker <jenfly@gmail.com>
2015-09-18 12:59:39 -0700

    Testing interp_latlon()

--- testing/testing-data-interp_latlon.py
+++ testing/testing-data-interp_latlon.py
@@ -7,7 +7,7 @@ import xray
 import atmos.utils as utils
 import atmos.plots as ap
 import atmos.data as dat
-from atmos.data import get_coord, latlon_equal, lon_convention, set_lon
+from atmos.data import get_coord, interp_latlon
 
 # ----------------------------------------------------------------------
 # NCEP2
@@ -24,51 +24,25 @@ lat = get_coord(ps, 'lat')
 lon = get_coord(ps, 'lon')
 plev = get_coord(u, 'plev')
 
-topo = dat.get_ps_clim(lat, lon) / 100
-topo.units = 'hPa'
-
 # ----------------------------------------------------------------------
-# MERRA
-# ----------------------------------------------------------------------
+# interp_latlon
 
-url = ('http://goldsmr3.sci.gsfc.nasa.gov/opendap/MERRA_MONTHLY/'
-    'MAIMCPASM.5.2.0/1979/MERRA100.prod.assim.instM_3d_asm_Cp.197901.hdf')
+lat_new = np.arange(-90, 90, 1.)
+lon_new = np.arange(0, 360, 1.)
 
-ds_m = xray.open_dataset(url)
-T_m = ds_m['T']
-ps_m = ds_m['PS']
-q_m = ds_m['QV']
-plev_m = get_coord(T_m, 'plev')
-lat_m = get_coord(T_m, 'lat')
-lon_m = get_coord(T_m, 'lon')
+# DataArray input
+ps_i = interp_latlon(ps, lat_new, lon_new)
 
-# ----------------------------------------------------------------------
-# latlon_equal, lon_convention, set_lon
-
-print(latlon_equal(ps, T))
-print(latlon_equal(T, T_m))
-
-print(lon_convention(lon))
-print(lon_convention(lon_m))
-
-ps_m2 = set_lon(ps_m, lonmax=360)
+# ndarray input
+ps_i2 = interp_latlon(ps.values, lat_new, lon_new, lat, lon)
 
 t = 0
-x1, y1 = np.meshgrid(lon_m, lat_m)
-x2, y2 = np.meshgrid(ps_m2.XDim, ps_m2.YDim)
 cmap='jet'
-ncontours = 30
 
-plt.figure(figsize=(7,8))
-plt.subplot(211)
-plt.contourf(x1, y1, ps_m[t].values, ncontours, cmap=cmap)
-plt.colorbar()
-plt.subplot(212)
-plt.contourf(x2, y2, ps_m2[t].values, ncontours, cmap=cmap)
-plt.colorbar()
-
-plt.figure(figsize=(7,8))
-plt.subplot(211)
-ap.pcolor_latlon(ps_m[t], cmap=cmap)
-plt.subplot(212)
-ap.pcolor_latlon(ps_m2[t], cmap=cmap)
+plt.figure(figsize=(7,10))
+plt.subplot(311)
+ap.pcolor_latlon(ps[t], cmap=cmap)
+plt.subplot(312)
+ap.pcolor_latlon(ps_i[t], cmap=cmap)
+plt.subplot(313)
+ap.pcolor_latlon(ps_i2[t], lat_new, lon_new, cmap=cmap)
