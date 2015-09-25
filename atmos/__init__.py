379a3df88e1165ec2e521501fd81deda9c3ed198
Jennifer Walker <jenfly@gmail.com>
2015-09-25 14:07:21 -0700

    Imports in init.py

--- atmos/__init__.py
+++ atmos/__init__.py
@@ -1,3 +1,76 @@
-import utils
-import data
-import plots
+from constants import const as constants
+
+from utils import (
+    print_if,
+    print_odict,
+    odict_insert,
+    odict_delete,
+    disptime,
+    strictly_increasing,
+    strictly_decreasing,
+    non_increasing,
+    non_decreasing,
+    month_str,
+    days_per_month,
+    season_months,
+    season_days,
+)
+
+from xrhelper import (
+    meta,
+    coords_init,
+    coords_assign,
+    ds_print,
+    ds_unpack,
+)
+
+from data import (
+    biggify,
+    nantrapz,
+    ncdisp,
+    ncload,
+    load_concat,
+    get_coord,
+    subset,
+    latlon_equal,
+    lon_convention,
+    set_lon,
+    interp_latlon,
+    mask_oceans,
+    mean_over_geobox,
+    pres_units,
+    pres_convert,
+    get_ps_clim,
+    correct_for_topography,
+    near_surface,
+    interp_plevels,
+    int_pres,
+)
+
+from variables import (
+    coriolis,
+    divergence_spherical_2d,
+    vorticity,
+    rossby_num,
+    potential_temp,
+    equiv_potential_temp,
+    moisture_flux_conv,
+    streamfunction,
+)
+
+from plots import (
+    degree_sign,
+    latlon_labels,
+    mapticks,
+    autoticks,
+    clevels,
+    init_latlon,
+    pcolor_latlon,
+    contourf_latlon,
+    contour_latlon,
+    init_latpres,
+    pcolor_latpres,
+    contourf_latpres,
+    contour_latpres,
+    contourf_timelat,
+)
