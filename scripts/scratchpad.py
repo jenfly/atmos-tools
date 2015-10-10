a8c9b55f23260b1533e54ae84e285213d637513a
Jennifer Walker <jenfly@gmail.com>
2015-10-09 18:58:57 -0700

    Added harmonic_decomp()

--- scripts/scratchpad.py
+++ scripts/scratchpad.py
@@ -16,6 +16,71 @@ from atmos.utils import print_if
 from atmos.constants import const as constants
 from atmos.data import get_coord
 
+# ----------------------------------------------------------------------
+# Harmonic analysis
+
+def harmonic_decomp(y, dt=1.0):
+    n = len(y)
+    T = float(n * dt)
+    nhalf = n // 2
+    ts = np.arange(1, n+1)
+    omega1 = 2 * np.pi / n
+
+    harmonics = {}
+    harmonics[0] = np.mean(y)
+    freqs = np.zeros(nhalf)
+    Ck = np.zeros(nhalf)
+    for k in range(1, nhalf + 1):
+        omega = k * omega1
+        A = (2.0/n) * np.sum(y * np.cos(omega * ts))
+        B = (2.0/n) * np.sum(y * np.sin(omega * ts))
+        freqs[k-1] = k / T
+        Ck[k-1] = np.sqrt(A**2 + B**2)
+        harmonics[k] = A*np.cos(omega*ts) + B*np.sin(omega*ts)
+
+    return freqs, Ck, harmonics
+
+N = 365
+dt = 1.0 / N
+x = np.arange(1, N+1)
+omega1 = 2 * np.pi / N
+y = 3.1 + 2.5 * np.sin(omega1 * x) + np.cos(2 * omega1 * x)
+
+freqs, Ck, harmonics = harmonic_decomp(y, dt)
+plt.figure(figsize=(7,8))
+plt.subplot(211)
+plt.plot(x,y)
+plt.plot(x, harmonics[1])
+plt.plot(x, harmonics[2])
+plt.plot(x, harmonics[0] + harmonics[1] + harmonics[2], '--')
+plt.subplot(212)
+plt.plot(freqs[:10], Ck[:10])
+
+datadir = '/home/jennifer/datastore/cmap/'
+with xray.open_dataset(datadir + 'cmap.precip.pentad.mean.nc') as ds:
+    ds.load()
+cmap = ds['precip'].sel(lat=11.25, lon=91.25)
+
+npentad = 73 # pentads/year
+dt = 5.0/365
+nyears = 3
+precip = cmap[:nyears*npentad]
+freqs, Ck, harmonics = harmonic_decomp(precip.values, dt)
+nharm = 12
+ypred = harmonics[0]
+for k in range(1,nharm+1):
+    ypred += harmonics[k]
+
+t = precip.time
+plt.figure(figsize=(7,8))
+plt.subplot(211)
+plt.plot(t,precip)
+for k in range(1, nharm+1):
+    plt.plot(t, harmonics[k])
+plt.plot(t, ypred, '--')
+plt.subplot(212)
+plt.plot(freqs[:10], Ck[:10])
+
 
 # ----------------------------------------------------------------------
 # Gradients
