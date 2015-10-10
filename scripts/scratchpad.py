f6f9a28acc499782733ba84443dd41eac5ae2391
Jennifer Walker <jenfly@gmail.com>
2015-10-10 12:26:13 -0700

    Version mucked up

--- scripts/scratchpad.py
+++ scripts/scratchpad.py
@@ -16,57 +16,40 @@ from atmos.utils import print_if
 from atmos.constants import const as constants
 from atmos.data import get_coord
 
+
+
 # ----------------------------------------------------------------------
 # Harmonic analysis
 
-def harmonic_coeffs(y):
+def harmonic_decomp(y, dt=1.0):
     n = len(y)
+    T = float(n * dt)
     nhalf = n // 2
-    t = np.arange(1, n+1)
+    ts = np.arange(1, n+1)
     omega1 = 2 * np.pi / n
 
-    Ak = np.zeros(nhalf + 1, dtype=float)
-    Bk = np.zeros(nhalf + 1, dtype=float)
-    Ak[0] = np.mean(y)
-    Bk[0] = 0.0
+    harmonics = {}
+    harmonics[0] = np.mean(y)
+    freqs = np.zeros(nhalf)
+    Ck = np.zeros(nhalf)
     for k in range(1, nhalf + 1):
         omega = k * omega1
-        Ak[k] = (2.0/n) * np.sum(y * np.cos(omega * t))
-        Bk[k] = (2.0/n) * np.sum(y * np.sin(omega * t))
+        A = (2.0/n) * np.sum(y * np.cos(omega * ts))
+        B = (2.0/n) * np.sum(y * np.sin(omega * ts))
+        freqs[k-1] = k / T
+        Ck[k-1] = np.sqrt(A**2 + B**2)
+        harmonics[k] = A*np.cos(omega*ts) + B*np.sin(omega*ts)
 
-    return Ak, Bk
+    return freqs, Ck, harmonics
 
-def harmonic_freqs(n, dt=1.0):
-    T = float(n * dt)
-    freqs = np.arange(n//2 + 1) / T
-    return freqs
-
-def harmonic_vals(y, Ak, Bk):
-    n = len(y)
-    t = np.arange(1, n+1)
-    omega1 = 2 * np.pi / n
-    harmonics = {}
-    for k, AB in enumerate(zip(Ak, Bk)):
-        A, B = AB
-        omega = k * omega1
-        harmonics[k] = A * np.cos(omega*t) + B * np.sin(omega*t)
-    return harmonics
-
-def harmonic_decomp(y, dt=1.0):
-    Ak, Bk = harmonic_coeffs(y)
-    Ck = np.sqrt(Ak**2 + Bk**2)
-    freqs = harmonic_freqs(len(y), dt)
-    harmonics = harmonic_vals(y, Ak, Bk)
-    return freqs, harmonics, Ak, Bk, Ck
-
-# ----------------------------------------------------------------------
+# -----------
 N = 365
 dt = 1.0 / N
 x = np.arange(1, N+1)
 omega1 = 2 * np.pi / N
 y = 3.1 + 2.5 * np.sin(omega1 * x) + np.cos(2 * omega1 * x)
 
-freqs, harmonics, Ak, Bk, Ck = harmonic_decomp(y, dt)
+freqs, Ck, harmonics = harmonic_decomp(y, dt)
 plt.figure(figsize=(7,8))
 plt.subplot(211)
 plt.plot(x,y)
@@ -76,16 +59,9 @@ plt.plot(x, harmonics[0] + harmonics[1] + harmonics[2], '--')
 plt.subplot(212)
 plt.plot(freqs[:10], Ck[:10])
 
-# Compare with FFT
-#Hk = np.fft.fft(y)
-Hk = np.fft.fftpack.rfft(y)
-Ak2, Bk2 = Hk.real, Hk.imag
-freqs2 = np.fft.fftfreq(len(y), dt)
-Ck2 = np.sqrt(Hk * Hk.conjugate())
-plt.figure()
-plt.plot(freqs2[:10], Ck2[:10])
 
-# ----------------------------------------------------------------------
+
+# -------------------
 datadir = '/home/jennifer/datastore/cmap/'
 with xray.open_dataset(datadir + 'cmap.precip.pentad.mean.nc') as ds:
     ds.load()
@@ -95,32 +71,27 @@ npentad = 73 # pentads/year
 dt = 5.0/365
 nyears = 3
 precip = cmap[:nyears*npentad]
-y = precip.values
-freqs, harmonics, Ak, Bk, Ck = harmonic_decomp(y, dt)
+freqs, Ck, harmonics = harmonic_decomp(precip.values, dt)
 ntrunc = 12
-ypred = np.zeros(len(y))
-ytrunc = np.zeros(len(y))
+ytrunc = harmonics[0]
+for k in range(1,nharm+1):
+    ytrunc += harmonics[k]
+ypred = np.zeros(len(precip))
 for k in harmonics:
     ypred += harmonics[k]
-    if k <= ntrunc:
-        ytrunc += harmonics[k]
 
 t = precip.time
-plt.figure(figsize=(14,8))
+plt.figure(figsize=(7,8))
 plt.subplot(211)
-plt.plot(t,y)
-for k in range(1, ntrunc+1):
+plt.plot(t,precip)
+for k in range(1, trunc+1):
     plt.plot(t, harmonics[k])
 plt.plot(t, ytrunc, '--')
-plt.plot(t, ypred, 'r--')
+plt.plot(t, ypred, '--')
 plt.subplot(212)
-plt.plot(freqs[:ntrunc], Ck[:ntrunc])
+plt.plot(freqs[:10], Ck[:10])
 
-# Compare with FFT
-Hk = np.fft.fft(y)
-Ak2, Bk2 = Hk.real, Hk.imag
-freqs2 = np.fft.fftfreq(len(y), dt)
-Ck2 = np.sqrt(Hk * Hk.conjugate())
+
 # ----------------------------------------------------------------------
 # Gradients
 
