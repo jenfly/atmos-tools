847932b65350ec62189d94826f3712580dfb4abe
Jennifer Walker <jenfly@gmail.com>
2017-09-16 19:20:03 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='atmos',
+      version='0.1',
+      description='Handy utilities for atmospheric science',
+      url='https://github.com/jenfly/atmos-tools',
+      author='Jennifer Walker',
+      author_email='jenfly@gmail.com',
+      license='MIT',
+      packages=['atmos'],
+      install_requires=[
+          'basemap',
+          'datetime',
+          'matplotlib',
+          'numpy',
+          'pandas',
+          'PyPDF2',
+          'scipy',
+          'xarray',
+      ],
+      include_package_data=True,
+      zip_safe=False,
+)
